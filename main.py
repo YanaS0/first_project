5cfa407a3a533abdd38d573ff72070a42d6babba
Yana <m1n.y4n@yandex.ru>
2026-08-12 16:39:39 +0300

    Почистила лишнее

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello from repository!')
+# Импорт load_dotenv.
+from dotenv import load_dotenv
+
+# Импорт библиотеки для работы с окружением.
+import os
+
+def print_author():
+
+        load_dotenv(dotenv_path='/Users/yana/pract/proj_git/a.env')
+        author = os.getenv('AUTHOR')
+        print(f"Автор проекта: {author}")
+print_author()
